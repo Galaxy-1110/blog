c3dcc7d17655b30cde2fad7bbfb9ee66ec51834c
Galaxy_ <suyogmaharjan38@gmail.com>
2025-03-14 10:43:38 +0545

    Formatting

--- destroy.php
+++ destroy.php
@@ -1,8 +1,8 @@
 <?php
 include_once("./components/alert.php");
    session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-session_start();
-alert("You have been logged out", "success", "./");
+   session_unset(); 
+   session_destroy(); 
+   session_start();
+   alert("You have been logged out", "success", "./");
 ?>
